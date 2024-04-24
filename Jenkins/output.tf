6e906c083c63a436e7248f4969e890c1c6bc7fd4
Soham Pawaskar <sohamp@umd.edu>
2024-04-24 15:14:33 -0400

    Remove all files

--- Jenkins/output.tf
+++
@@ -1,5 +0,0 @@
-/*
-output "Jenkins_server_IP" {
-  value = aws_instance.my_instance.public_ip
-}
-*/
--- Jenkins/run.ps1
+++
@@ -1,8 +0,0 @@
-# Initialize Terraform
-terraform init
-
-# Display the execution plan
-terraform plan
-
-# Apply the Terraform configuration
-terraform apply -auto-approve
