62d3852d2621fa340cdbbe927dd66d619be2624a
Soham Pawaskar <sohamp@umd.edu>
2024-04-28 18:16:30 -0400

    Version3.5

--- Tier2_Application/main.tf
+++ Tier2_Application/main.tf
@@ -20,6 +20,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#VPC
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -27,7 +29,8 @@ module "vpc" {
   cidr = var.vpc_cidr
 
   azs             = data.aws_availability_zones.azs.names
-  private_subnets = var.private_subnets
+  private_subnets = var.all_private_subnets
+  
   public_subnets  = var.public_subnets
 
   enable_dns_hostnames = true
@@ -48,4 +51,69 @@ module "vpc" {
   }
 }
 
+
+#SG:
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "SecurityG-EC2"
+  description = "Security group for EC2 Server"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  egress_with_cidr_blocks =[
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      description = "All"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = {
+    Name= "EC-SG"
+  }
+}
+
+
+
+
+#EC2:
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "EC2-Server"
+
+  instance_type          = var.instance_type
+  #key_name               = 
+  monitoring             = true
+  vpc_security_group_ids = [module.sg.security_group_id]
+  subnet_id              = module.vpc.private_subnets[0:2]
+#   associate_public_ip_address = true
+#   user_data = file("jenkins-installation.sh")
+  availability_zone = data.aws_availability_zones.azs.names
+
+  tags = {
+    Name = "EC2-server"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
 
--- Tier2_Application/variables.tf
+++ Tier2_Application/variables.tf
@@ -8,7 +8,22 @@ variable "public_subnets" {
   type = list(string)
 }
 
-variable "private_subnets" {
+variable "private_subnets_web" {
   description = "Private subnet list"
   type = list(string)
+}
+
+variable "private_subnets_db" {
+  description = "Private subnet list"
+  type = list(string)
+}
+
+variable "all_private_subnets" {
+  type = list(string)
+  default = concat(var.private_subnets_web, var.private_subnets_db)
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+  type = string
 }
