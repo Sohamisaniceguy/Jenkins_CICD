d5814900dc06d2593d2e09201316c4575afe69ab
Soham Pawaskar <sohamp@umd.edu>
2024-04-29 16:00:17 -0400

    SG

--- Tier2_Application/sg.tf
+++ Tier2_Application/sg.tf
@@ -84,42 +84,34 @@ module "ec2_sg" {
 }
 
 
-# module "db_sg" {
-#   source = "terraform-aws-modules/security-group/aws"
+#DB:
+module "db_sg" {
+  source = "terraform-aws-modules/security-group/aws"
 
-#   name        = "SecurityG-DB"
-#   description = "Security group for DB"
-#   vpc_id      = module.vpc.vpc_id
+  name        = "SecurityG-DB"
+  description = "Security group for DB"
+  vpc_id      = module.vpc.vpc_id
+  security_group_id = module.ec2_sg.security_group_id
 
-#   ingress_with_source_security_group_id = [
-#     {
-#       from_port   = 3360
-#       to_port     = 3360
-#       protocol    = "tcp"
-#       description = "HTTP"
-#     #   cidr_blocks = "0.0.0.0/0"
-#       security_group=module.ec2_sg.security_group_id
-#     },
-#     {
-#       from_port   = 22
-#       to_port     = 22
-#       protocol    = "tcp"
-#       description = "HTTP"
-#       cidr_blocks = "0.0.0.0/0"
-#     },
-#   ]
+  ingress_with_source_security_group_id = [
+    {
+      description              = "HTTP from service one"
+      rule                     = "mysql-tcp"
+      source_security_group_id = module.ec2_sg.security_group_id
+    },
+  ]
 
-#   egress_with_cidr_blocks =[
-#     {
-#       from_port   = 0
-#       to_port     = 0
-#       protocol    = "-1"
-#       description = "All"
-#       cidr_blocks = "0.0.0.0/0"
-#     }
-#   ]
+  egress_with_cidr_blocks =[
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      description = "All"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
 
-#   tags = {
-#     Name= "DB-SG"
-#   }
-# }
+  tags = {
+    Name= "DB-SG"
+  }
+}
