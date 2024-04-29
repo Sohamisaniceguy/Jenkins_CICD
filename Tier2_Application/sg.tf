8436dcfd07873ebc29fa35e3bbc46121e602d8e6
Soham Pawaskar <sohamp@umd.edu>
2024-04-29 15:27:09 -0400

    SG

--- Tier2_Application/sg.tf
+++ Tier2_Application/sg.tf
@@ -56,46 +56,37 @@ module "ec2_sg" {
 
   ingress_with_source_security_group_id = [
     {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      description = "HTTP"
-    #   cidr_blocks = "0.0.0.0/0"
-    security_group_id= module.alb_sg.security_group_id
-    source_security_group_id = module.alb_sg.security_group_id
+      description              = "HTTP from service one"
+      rule                     = "http-80-tcp"
+      source_security_group_id = module.alb_sg.security_group_id
     },
     {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "HTTPS"
-    #   cidr_blocks = "0.0.0.0/0"
-    security_group_id= module.alb_sg.security_group_id
-    source_security_group_id = module.alb_sg.security_group_id
+      description              = "HTTPS from service one"
+      rule                     = "https-443-tcp"
+      source_security_group_id = data.alb_sg.security_group_id
     },
     {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      description = "SSH"
-      cidr_blocks = "0.0.0.0/0"
-    },
+      description           = "SSH from all"
+      rule                  = "ssh-22-tcp"
+      cidr_blocks           = ["0.0.0.0/0"]
+    }
   ]
 
-  egress_with_cidr_blocks =[
+  egress_with_cidr_blocks = [
     {
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
       description = "All"
-      cidr_blocks = "0.0.0.0/0"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   ]
 
   tags = {
-    Name= "EC2-SG"
+    Name = "EC2-SG"
   }
 }
+
 
 # module "db_sg" {
 #   source = "terraform-aws-modules/security-group/aws"
