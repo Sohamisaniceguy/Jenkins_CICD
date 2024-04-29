5e35669c5ec14f73dc6d205c8c28549b8ea6f8bb
Soham Pawaskar <sohamp@umd.edu>
2024-04-29 13:27:05 -0400

    SG

--- Tier2_Application/sg.tf
+++ Tier2_Application/sg.tf
@@ -1,49 +1,49 @@
 # #ALB - SG:
-# module "alb_sg" {
-#   source = "terraform-aws-modules/security-group/aws"
+module "alb_sg" {
+  source = "terraform-aws-modules/security-group/aws"
 
-#   name        = "SecurityG-ALB"
-#   description = "Security group for ALB"
-#   vpc_id      = module.vpc.vpc_id
+  name        = "SecurityG-ALB"
+  description = "Security group for ALB"
+  vpc_id      = module.vpc.vpc_id
 
-#   ingress_with_cidr_blocks = [
-#     {
-#       from_port   = 80
-#       to_port     = 80
-#       protocol    = "tcp"
-#       description = "HTTP"
-#       cidr_blocks = "0.0.0.0/0"
-#     },
-#     {
-#       from_port   = 443
-#       to_port     = 443
-#       protocol    = "tcp"
-#       description = "HTTPS"
-#       cidr_blocks = "0.0.0.0/0"
-#     },
-#     {
-#       from_port   = 22
-#       to_port     = 22
-#       protocol    = "tcp"
-#       description = "HTTP"
-#       cidr_blocks = "0.0.0.0/0"
-#     },
-#   ]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "HTTPS"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
 
-#   egress_with_cidr_blocks =[
-#     {
-#       from_port   = 0
-#       to_port     = 0
-#       protocol    = "-1"
-#       description = "All"
-#       cidr_blocks = "0.0.0.0/0"
-#     }
-#   ]
+  egress_with_cidr_blocks =[
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      description = "All"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
 
-#   tags = {
-#     Name= "ALB-SG"
-#   }
-# }
+  tags = {
+    Name= "ALB-SG"
+  }
+}
 
 
 # #EC2:
