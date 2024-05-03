b7f2abeaf701c5d81c0a712b22305eca9d8ca111
Soham Pawaskar <sohamp@umd.edu>
2024-05-03 13:25:17 -0400

    ALB-ASG

--- Tier2_Application/alb.tf
+++ Tier2_Application/alb.tf
@@ -1,110 +1,31 @@
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+# module "alb" {
+#   source = "terraform-aws-modules/alb/aws"
 
-  name    = "Tier2App-alb"
-  vpc_id  = module.vpc.vpc_id
-  subnets = module.vpc.public_subnets
+#   name    = "Tier2App-alb"
+#   vpc_id  = module.vpc.vpc_id
+#   subnets = module.vpc.public_subnets
 
-  security_groups = [module.alb_sg.security_group_id]
-  enable_deletion_protection=false #This is True by default
+#   security_groups = [module.alb_sg.security_group_id]
+#   enable_deletion_protection=false #This is True by default
 
-#   access_logs = {
-#     bucket = "Tier2App-alb-logs"
-#   }
+# #   access_logs = {
+# #     bucket = "Tier2App-alb-logs"
+# #   }
 
-listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-
-      rules = {
-        ex-fixed-response = {
-          priority = 3
-          actions = [{
-            type         = "fixed-response"
-            content_type = "text/plain"
-            status_code  = 200
-            message_body = "This is a fixed response"
-          }]
-
-          conditions = [{
-            http_header = {
-              http_header_name = "x-Gimme-Fixed-Response"
-              values           = ["yes", "please", "right now"]
-            }
-          }]
-        }
-
-
-#   target_groups = {
-#     ex-instance = {
-#       name                              = "Tier2App-EC2-TG"
-#       protocol                          = "HTTP"
-#       port                              = 80
-#       target_type                       = "instance"
-#       vpc_id                            = module.vpc.vpc_id
-      
-
-#       health_check = {
-#         enabled             = true
-#         interval            = 300
-#         path                = "/healthz"
-#         port                = "traffic-port"
-#         healthy_threshold   = 2
-#         unhealthy_threshold = 5
-#         timeout             = 60
-#         matcher             = "200"
-#       }
-
-#       protocol_version = "HTTP1"
-#       target_id        = aws_instance.this.id # Problem
-#       port             = 80
-#       tags = {
-#         InstanceTargetGroupTag = "EC2-clientTG"
-#       }
-#     }
-
-
-#   tags = {
-#     Name= "Tier2App-ALB"
-#     Environment = "dev"
-#     # Project     = "Example"
-#   }
+# tags = {
+#   Name = "Tier2App-ALB"
 # }
 
+# }
 
+# #Listner:
+# resource "aws_lb_listener" "alb_http_listner" {
+#   load_balancer_arn = module.alb.arn
+#   port              = 80
+#   protocol          = "HTTP"
 
-target_groups = [
-    {
-      backend_protocol        = "HTTP"
-      backend_port            = 80
-      target_type             = "instance"
-      health_check_path       = "/"
-      health_check_port       = "80"
-      health_check_protocol   = "HTTP"
-      health_check_timeout    = 5
-      health_check_interval   = 30
-      healthy_threshold       = 2
-      unhealthy_threshold     = 2
-      matcher                 = "200"
-      name                    = "my-target-group"
-      deregistration_delay    = 300
-    #   stickiness_cookie_duration = 3600
-    #   stickiness_type         = "lb_cookie"
-    }
-  ]
-
-  
-
-
-}
-
-    }
-
-}
-}
+#   default_action {
+#     type = "forward"
+#     target_group_arn = aws_lb_target_group.alb_target_group.arn
+#   }
+# }
--- Tier2_Application/asg.tf
+++ Tier2_Application/asg.tf
@@ -1,53 +1,53 @@
-#Key Pair passing:
-module "key_pair" {
-  source = "terraform-aws-modules/key-pair/aws"
+# #Key Pair passing:
+# module "key_pair" {
+#   source = "terraform-aws-modules/key-pair/aws"
 
-  key_name = "ASG-Key"
-  public_key = file("key1.pem")
-}
+#   key_name = "ASG-Key"
+#   public_key = file("key1.pem")
+# }
 
-#Autoscaling Configuration:
-module "asg" {
-  source  = "terraform-aws-modules/autoscaling/aws"
+# #Autoscaling Configuration:
+# module "asg" {
+#   source  = "terraform-aws-modules/autoscaling/aws"
 
-  # Autoscaling group
-  name = "2Tier-asg"
+#   # Autoscaling group
+#   name = "2Tier-asg"
 
-  min_size                  = 1
-  max_size                  = 2
-  desired_capacity          = 1
-  wait_for_capacity_timeout = 0
-  health_check_type         = "ELB" #'ELB' or default EC2
-  vpc_zone_identifier       = slice(module.vpc.private_subnets,0,2)
-#   target_group_arns         = [module.alb.lb_tg_arn]
+#   min_size                  = 1
+#   max_size                  = 2
+#   desired_capacity          = 1
+#   wait_for_capacity_timeout = 0
+#   health_check_type         = "ELB" #'ELB' or default EC2
+#   vpc_zone_identifier       = slice(module.vpc.private_subnets,0,2)
+# #   target_group_arns         = [module.alb.lb_tg_arn]
   
 
 
-  instance_refresh = {
-    strategy = "Rolling"
-    preferences = {
-      checkpoint_delay       = 600
-      checkpoint_percentages = [35, 70, 100]
-      instance_warmup        = 300
-      min_healthy_percentage = 50
-      max_healthy_percentage = 100
-    }
-    triggers = ["tag"]
-  }
+#   instance_refresh = {
+#     strategy = "Rolling"
+#     preferences = {
+#       checkpoint_delay       = 600
+#       checkpoint_percentages = [35, 70, 100]
+#       instance_warmup        = 300
+#       min_healthy_percentage = 50
+#       max_healthy_percentage = 100
+#     }
+#     triggers = ["tag"]
+#   }
 
-  # Launch template
-  launch_template_name        = var.instance_name
-  launch_template_description = "Launch template for ASG"
-  image_id          = var.image_id
-  instance_type     = var.instance_type
-#   key_name          = module.key_pair.key_pair_name 
-  ebs_optimized     = true
-  enable_monitoring = true
-  user_data = base64encode(file("config_ec2.sh"))
+#   # Launch template
+#   launch_template_name        = var.instance_name
+#   launch_template_description = "Launch template for ASG"
+#   image_id          = var.image_id
+#   instance_type     = var.instance_type
+# #   key_name          = module.key_pair.key_pair_name 
+#   ebs_optimized     = true
+#   enable_monitoring = true
+#   user_data = base64encode(file("config_ec2.sh"))
   
   
-  tags = {
-    Name = "EC2-ASG"
-    Environment = "dev"
-  }
-}
+#   tags = {
+#     Name = "EC2-ASG"
+#     Environment = "dev"
+#   }
+# }
