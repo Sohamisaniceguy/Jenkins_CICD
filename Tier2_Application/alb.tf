81d5d56d8336e8ef60d9d27c5a6847c54eba0076
Soham Pawaskar <sohamp@umd.edu>
2024-05-06 15:48:12 -0400

    ALB-ASG

--- Tier2_Application/alb.tf
+++ Tier2_Application/alb.tf
@@ -1,31 +1,56 @@
-# module "alb" {
-#   source = "terraform-aws-modules/alb/aws"
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
 
-#   name    = "Tier2App-alb"
-#   vpc_id  = module.vpc.vpc_id
-#   subnets = module.vpc.public_subnets
+  name    = "Tier2App-alb"
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
 
-#   security_groups = [module.alb_sg.security_group_id]
-#   enable_deletion_protection=false #This is True by default
+  security_groups = [module.alb_sg.security_group_id]
+  enable_deletion_protection=false #This is True by default
 
-# #   access_logs = {
-# #     bucket = "Tier2App-alb-logs"
-# #   }
+  # access_logs = {
+  #   bucket = "Tier2App-alb-logs"
+  # }
 
-# tags = {
-#   Name = "Tier2App-ALB"
-# }
+tags = {
+  Name = "Tier2App-ALB"
+}
 
-# }
+}
 
-# #Listner:
-# resource "aws_lb_listener" "alb_http_listner" {
-#   load_balancer_arn = module.alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
+#Listner:
+resource "aws_lb_listener" "alb_http_listner" {
+  load_balancer_arn = module.alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-#   default_action {
-#     type = "forward"
-#     target_group_arn = aws_lb_target_group.alb_target_group.arn
-#   }
-# }
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
+
+#Target Group:
+resource "alb_lb_target_group" "alb_target_group" {
+  name= "Tier2App-alb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = module.vpc.vpc_id
+
+  health_check {
+    enabled             = true
+    interval            = 300
+    path                = "/"
+    timeout             = 60
+    matcher             = 200
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
