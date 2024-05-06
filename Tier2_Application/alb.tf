2a4878e59270d4ab0a44c80bb156d284e85881ea
Soham Pawaskar <sohamp@umd.edu>
2024-05-06 15:55:18 -0400

    ALB-ASG

--- Tier2_Application/alb.tf
+++ Tier2_Application/alb.tf
@@ -18,39 +18,39 @@ tags = {
 
 }
 
-#Listner:
-resource "aws_lb_listener" "alb_http_listner" {
-  load_balancer_arn = module.alb.arn
-  port              = 80
-  protocol          = "HTTP"
+# #Listner:
+# resource "aws_lb_listener" "alb_http_listner" {
+#   load_balancer_arn = module.alb.arn
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.alb_target_group.arn
-  }
-}
+#   default_action {
+#     type = "forward"
+#     target_group_arn = aws_lb_target_group.alb_target_group.arn
+#   }
+# }
 
-#Target Group:
-resource "alb_lb_target_group" "alb_target_group" {
-  name= "Tier2App-alb-tg"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "instance"
-  vpc_id      = module.vpc.vpc_id
+# #Target Group:
+# resource "alb_lb_target_group" "alb_target_group" {
+#   name= "Tier2App-alb-tg"
+#   port        = 80
+#   protocol    = "HTTP"
+#   target_type = "instance"
+#   vpc_id      = module.vpc.vpc_id
 
-  health_check {
-    enabled             = true
-    interval            = 300
-    path                = "/"
-    timeout             = 60
-    matcher             = 200
-    healthy_threshold   = 2
-    unhealthy_threshold = 5
-  }
+#   health_check {
+#     enabled             = true
+#     interval            = 300
+#     path                = "/"
+#     timeout             = 60
+#     matcher             = 200
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 5
+#   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 
-}
+# }
 
