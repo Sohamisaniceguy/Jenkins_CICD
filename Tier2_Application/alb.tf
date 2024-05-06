3e78a3d270b4d7cb0e146ebfbf88bbac86ffb154
Soham Pawaskar <sohamp@umd.edu>
2024-05-06 17:51:02 -0400

    ALB-ASG

--- Tier2_Application/alb.tf
+++ Tier2_Application/alb.tf
@@ -12,38 +12,44 @@ module "alb" {
   #   bucket = "Tier2App-alb-logs"
   # }
 
-# listeners = {
-#     ex-http-https-redirect = {
-#       port     = 80
-#       protocol = "HTTP"
-#       redirect = {
-#         port        = "443"
-#         protocol    = "HTTPS"
-#         status_code = "HTTP_301"
-#       }
-#     }
-#     ex-https = {
-#       # port            = 443
-#       # protocol        = "HTTPS"
-#       # certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+listeners = {
+    ex-http-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
 
-#       forward = {
-#         target_group_key = "ex-instance"
-#       }
-#     }
+target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
 
-# target_groups = {
-#     ex-instance = {
-#       name_prefix      = "h1"
-#       protocol         = "HTTP"
-#       port             = 80
-#       target_type      = "instance"
-#     }
-#   }
+      health_check = {
+    enabled             = true
+    interval            = 300
+    path                = "/"
+    timeout             = 60
+    matcher             = 200
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
+  }
+
+  lifecycle = {
+    create_before_destroy = true
+  }
+
+    }
+  }
 
 
 tags = {
   Name = "Tier2App-ALB"
+}
+
 }
 
 }
